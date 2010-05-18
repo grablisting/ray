2eab8536ff6a6a93a082dd7b545e3d5f29e2acb4
Ray Scott Peters <peter204@CF405-22.cs.wwu.edu>
2010-05-18 13:57:16 -0700

    assg4 #1 almsot done

--- cs347/hw4/mstrebl.asm
+++ cs347/hw4/mstrebl.asm
@@ -16,14 +16,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 %include "asm_io.inc"
-segment .data
 
-NEWLINE_FLAG    equ    0Ah
-MAX_LENGTH    equ    80
-BAD_INPUT    db    "            Hello     there:",0
+segment .data
+NEWLINE_FLAG		equ	0Ah
+MAX_LENGTH		equ	80
+
+characters_left_msg	db	", characters left ",0
+input_loop_msg		db	"Entering input_loop",0
+clean_input_msg		db	"Entering clean_input",0
+skip_blanks_msg		db	"Entering skip_blanks",0
+read_space_msg		db	"a space",0
+read_printable_msg	db	"Entering read_printable on ",0
+fin_msg			db	"Entering fin",0
 
 segment .bss
-input        resd 1
+input			resd 1
 
 segment .text
         global  asm_main
@@ -33,62 +40,94 @@ asm_main:
         pusha
 
 start_mstrebl:
-    mov    ECX, MAX_LENGTH
-    ;mov    EBX, BAD_INPUT
-    jmp    clean_input
+	mov	ECX, MAX_LENGTH		;Initialize maximum string length counter
+	mov	EBX, input		
 
 input_loop:
-    call    read_char
-    mov    [EBX], AL
-    inc    EBX
-    cmp    AL, NEWLINE_FLAG
-    je    clean_input
-    loop    input_loop
+	call	read_char
+	mov	[EBX], AL
+	inc	EBX
+	cmp	AL, NEWLINE_FLAG
+	je	clean_input
+	loop	input_loop
 
 clean_input:
-    mov    EBX, BAD_INPUT
-    mov    EAX, MAX_LENGTH
-    sub    EAX, ECX
-    mov    ECX, EAX
-    jmp    skip_blanks
+	push	EAX
+	mov	EAX, clean_input_msg
+	call	print_string
+	call	print_nl
+	pop	EAX
+
+	mov	EAX, MAX_LENGTH		;Load max_length allowable
+	sub	EAX, ECX		;Subtract unused positions
+	call	print_int
+	call	print_nl
+	mov	ECX, EAX		;Store number of used positions
+	mov	EBX, input		;Load a string location
+	jmp	skip_blanks
 
 get_next:
-    cmp    ECX, 0
-    je    fin
-    mov    AL, [EBX]
-    cmp    AL, NEWLINE_FLAG
-    je    fin
-    inc    EBX
-    dec    ECX
-    ret
+	dec	ECX			;Record retrieval
+	mov	AL, [EBX]		;Otherwise, load a character
+	inc 	EBX			;Point at the next character
+	ret
 
 skip_blanks:
-    call    get_next
-    cmp    AL, 32
-    jg    read_printable
-    jmp    skip_blanks
-    
+	push	EAX
+	mov	EAX, skip_blanks_msg
+	call	print_string
+	call	print_nl
+	pop	EAX
+
+	call	get_next		;Get the next character
+	cmp	AL, 33			;If > 32, it is printable
+	jge	read_printable		;If true, print it!
+	jmp	skip_blanks		;Otherwise, throw it out and continue
+	
 read_space:
-    call    print_input
-    call    get_next
-    cmp    AL, 33
-    jl    skip_blanks
+	push	EAX
+	mov	EAX, read_space_msg
+	call	print_string
+	pop	EAX
+
+	call	print_input		;Display current character
+	call	get_next		;Get the next one
+	cmp	AL, 33			;If < 33, it is not printable
+	jl	skip_blanks		;If true, skip blanks
 
 read_printable:
-    call    print_input
-    call    get_next
-    cmp    AL, 33
-    jl    read_space
-    jmp    read_printable
-    
+	push	EAX
+	mov	EAX, read_printable_msg
+	call	print_string
+	pop	EAX
+
+	cmp	AL, 33			;If < 33, it is not printable
+	jl	read_space		;If true, print it!
+	call	print_input		;Display current character
+	call	get_next		;Get the next one
+	jmp	read_printable		;Otherwise, continue reading_printable
 
 print_input:
-    call    print_char
-    ret
-    
+	call	print_char
+	push	EAX
+	mov	EAX,characters_left_msg
+	call	print_string
+	xchg	ECX, EAX
+	call	print_int
+	xchg	ECX, EAX
+	pop	EAX
+	call	print_nl
+	cmp	ECX, 0
+	je	fin
+	ret
+
 fin:
-    call    print_nl
+	dump_regs 1
         popa
         mov     EAX, 0
-        leave                     
+        leave
         ret
+
+
+
+
