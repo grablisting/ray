dc4a9a22e6d3f72caf9d1066b49db3f300512427
Ray Scott Peters <peter204@quest-desktop.(none)>
2010-05-21 00:50:30 -0700

    cs347hw complete

--- cs347/hw4/mstrebl.asm
+++ cs347/hw4/mstrebl.asm
@@ -2,40 +2,39 @@
 ;
 ; file: mstrebl.asm
 ;
-; This program reads an input stream and discards both leading
-; and duplicate blank characters.
+; This program reads an input stream and then calls a strip blanks
+; procedure.
 ;
 ; Input: A string with a maximum of 80 characters.
 ;
-; Output: The input string without leading nor duplicate blank characters.
+; Output: Displays the new string without leading or duplicate
+; 	blank characters.
 ;
 ; Ray Peters
 ;
-; May 19, 2010
+; May 21, 2010
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 %include "asm_io.inc"
 
 segment .data
-NF		equ	0Ah
-MAX_LENGTH	equ	80
+	NF	equ	0Ah
+	MAX	equ	80
 
 segment .bss
-total		resd 	1
-input		resw	80
-output		resw	80
+	input	resb	80
 
 segment .text
         global  asm_main
-	;extern	strebl
+	extern	strebl
 
 asm_main:
         enter   0,0
         pusha
 
 start_mstrebl:
-	mov	ECX, MAX_LENGTH
+	mov	ECX, MAX
 	mov	EBX, input
 	mov	EAX, 0
 
@@ -44,62 +43,26 @@ input_loop:
 	mov	[EBX], AL
 	inc	EBX
 	cmp	AL, NF
-	je	init_clean
+	je	clean_input
 	loop	input_loop
 
-init_clean:
-	mov	EAX, MAX_LENGTH
-	sub	EAX, ECX
-	xchg	ECX, EAX
-	add	ECX, ECX
-
-	mov	EAX, 0
-	mov	EBX,input
-	mov	EDX,output
-
-skip_blanks:
-	call	get_next_char
-	cmp	AL, 33
-	jl	skip_blanks
-	jmp	clean_input
-
 clean_input:
-	call	save_this_char
-	call	get_next_char
-	cmp	AL, 33
-	jl	save_a_space
-	loop	clean_input
-	jmp	fin
+	mov	EAX, MAX
+	sub	EAX, ECX
+	add	EAX, EAX
+	push	EAX
 
-get_next_char:
-	cmp	ECX,0
-	jle	fin
-	mov	AL, [EBX]
-	inc	EBX
-	dec	ECX
-	ret
+	mov	EAX, dword input
+	push	EAX
 
-save_a_space:
-	call	save_this_char
-	call	get_next_char
-	cmp	AL, 33
-	jge	clean_input
-	jmp	skip_blanks
-
-save_this_char:
-	mov	[EDX], AL
-	inc	EDX
-	cmp	AL,NF
-	je	fin
-	ret
+	call	strebl
+	add	esp, 8
 
 fin:
-	mov	EAX, output
-	call	print_string
-
         popa
         mov     EAX, 0
 	leave
+	ret
 
 
 
--- cs347/hw4/strebl.asm
+++ cs347/hw4/strebl.asm
@@ -1,20 +1,39 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+; file: strebl.asm
+;
+; This program takes a string then removes leading
+; and duplicate blank characters.
+;
+; Input: A string length and then a string pointer on the stack.
+;
+; Output: Replaces the old string with the new string.
+;
+; Ray Peters
+;
+; May 21, 2010
+;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 %include "asm_io.inc"
+%define	input	ebp + 8
+%define length	ebp + 12
 
-segment .data
+segment .data	
+	NF	equ	0Ah
+
+segment .bss
+	temp	resb	80
+
 segment .text
 	global	strebl
 
 strebl:
-	enter	0,0
-
-	mov	EAX, MAX_LENGTH
-	xchg	ECX, EAX
-	sub	ECX, EAX
-	mov	[total], ECX
-
-	mov	EBX,input
-	mov	EDX,output
+        enter   0, 0
+        mov     ECX, [length]
+	mov	EAX, 0
+	mov	EBX, [input]
+	mov	EDX, temp
 
 skip_blanks:
 	call	get_next_char
@@ -28,9 +47,15 @@ clean_input:
 	cmp	AL, 33
 	jl	save_a_space
 	loop	clean_input
-
-	mov	EDX, output
 	jmp	fin
+
+get_next_char:
+	cmp	ECX,0
+	jle	fin
+	mov	AL, [EBX]
+	inc	EBX
+	dec	ECX
+	ret
 
 save_a_space:
 	call	save_this_char
@@ -39,12 +64,30 @@ save_a_space:
 	jge	clean_input
 	jmp	skip_blanks
 
-get_next_char:
-	mov	AL, [EBX]
-	inc	EBX
-	ret
-
 save_this_char:
 	mov	[EDX], AL
 	inc	EDX
+	cmp	AL, NF
+	je	clean_up
 	ret
+
+clean_up:
+	mov	EBX, [input]
+	mov	EDX, temp
+	sub	[length], ECX
+	mov	ECX, [length]
+
+copy:
+	mov	AL, [EDX]
+	call	print_char
+	mov	[EBX], AL
+	inc	EBX
+	inc	EDX
+	loop	copy
+
+fin:
+	leave
+	ret
+
+
+
